aa13b49f6fb72d8235fc702aff61c9f10379a4c5
Sanmir Cruz <sancruz.dev@gmail.com>
2022-01-12 15:26:33 -0300

    Configurando o App.js, e atualizando o arquivo netlify

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+
+function App() {
+   fetch(process.env.REACT_APP_API_URL || 'http://localhost:8080/api/v1')
+      .then(() => console.log("DEU CERTO !!!"))
+      .catch(() => console.log("DEU ERRO"))
+
+   return (
+      <div className="App">
+         <header className="App-header">
+            <p>
+               Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+               className="App-link"
+               href="https://reactjs.org"
+               target="_blank"
+               rel="noopener noreferrer"
+            >
+               Learn React
+            </a>
+         </header>
+      </div>
+   );
+}
+
+export default App;
