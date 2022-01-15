35753f4e43138f4d87ef43af936f5be013f29130
Sanmir Cruz <sancruz.dev@gmail.com>
2022-01-15 15:30:38 -0300

    Props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -9,16 +9,26 @@ function Listing() {
 
    // const [estado interno, função que altera esse estado] 
    const [pageNumber, setPageNumber] = useState(0);
+   const [page, setPage] = useState<MoviePage>({
+      content: [],
+      last: true,
+      totalPages: 0,
+      totalElements: 0,
+      size: 12,
+      number: 0,
+      first: true,
+      numberOfElements: 0,
+      empty: true
+   });
 
    useEffect(() => {
-      axios.get(`${BASE_URL}/movies/?size=12&page=1`)
+      // Parâmetro de ordernação (sort=id): Isso garante que a busca vai ser ordenada por id (os filmes virão na mesma ordem). Assim como o 'id', pode-se fazer o mesmo com o title.
+      axios.get(`${BASE_URL}/movies/?size=12&page=${pageNumber}&sort=id`)
          .then(response => {
             const data = response.data as MoviePage;
-            console.log(response.data);
-            setPageNumber(data.number);
+            setPage(data);
          });
-   }, []);
-
+   }, [pageNumber]);
 
 
    // FORMA ERRADA (TESTE)
@@ -26,33 +36,28 @@ function Listing() {
    //    .then(response => {
    //       // .data: corpo da resposta 
    //       console.log(response.data);
+   //       setPageNumber(data.number);
    //    });
 
    return (
       <>
-      <p>{pageNumber}</p>
          <Pagination />
 
          <div className="container">
             <div className="row">
-               <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                  <MovieCard />
-               </div>
-               <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                  <MovieCard />
-               </div>
-               <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                  <MovieCard />
-               </div>
-               <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                  <MovieCard />
-               </div>
-               <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                  <MovieCard />
-               </div>
-               <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                  <MovieCard />
-               </div>
+
+               {/* RENDERIZAÇÃO DINÂMICA DE COLEÇÃO
+                  1. map permite executar cada coisa com cada item no array (no filme) 
+                  2. NOTA: em uma renderização dinâmica de coleção, cada elemento renderizado DEVE possuir um atributo key. ex(key={movie.id}).
+               */}
+               {page.content.map(movie => (
+                  <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                     <MovieCard movie={movie} />
+                  </div>
+               )
+               )}
+
+
             </div>
          </div>
 
